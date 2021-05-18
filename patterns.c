36aa993553292260c20014e8260d4c1b6ab99196
Nived Kannada <nivedkannada@users.noreply.github.com>
2021-05-18 19:19:52 +0530

    Debugged

--- patterns.c
+++ patterns.c
@@ -3,7 +3,7 @@
 int main(void)
 {
 	int type = get_int("Enter type of pattern (1 or 2) ");
-	int num_rows = get_int("Number of rows ");
+	int num_rows = get_int("Number of rows");
 	int c=num_rows;
 	switch(type)
 	{
